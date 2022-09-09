b3f1919ab2a4b53c3d0485e3701d5bd882bdd8dc
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-09 14:52:41 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (succes/correct)
+ * Return: Always 0 (success/correct)
  */
 
 int main(void)
 {
 	char alphabet;
+
 	for (alphabet = 122; alphabet >= 93; alphabet++)
 	{
 		putchar (alphabet);
