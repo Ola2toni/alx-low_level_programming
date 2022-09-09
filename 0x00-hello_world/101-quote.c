787df486be278d165e44ac4132390bb741da0349
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-09 08:33:26 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
 
 /*
  * main - Prints "and the piece of art is useful" -Dora Korpar, 2015- 10- 19",
- *            followed by a new line,to standard error.
+ *           followed by a new line,
+ to standard error.
  *
  *            Return: Always 1.
  */
