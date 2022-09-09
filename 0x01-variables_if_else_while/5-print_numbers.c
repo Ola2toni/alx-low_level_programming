79f568910480e6f7564aacbe6d4ee53fc1bab5dd
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-09 13:52:44 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf ("%i", i);
+	}
+
+	print f ("\n");
+	return (0);
+}
