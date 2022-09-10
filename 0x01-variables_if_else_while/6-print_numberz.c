b0a44f3050c14bac4ff6cef5f2e589c41672453f
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-10 14:48:24 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	int n;
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	putchar('\n');
+	return (0);
+}
