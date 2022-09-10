b1689bb7b03961746e621e6de51b3f82646b1d74
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-10 13:40:52 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	char base_16;
-        for (base_16 = '0'; base_16 <= '9'; base_16++)
-	{
+char base_16;
+for (base_16 = '0'; base_16 <= '9'; base_16++)
+{
 		putchar (base_16);
-	}
-	for (base_16 = 'a'; base_16 <= 'f'; base_16++)
-	{
+}
+for (base_16 = 'a'; base_16 <= 'f'; base_16++)
+{
 		putchar (base_16);
-	}
-	putchar ('\n');
-	return (0);
+}
+putchar ('\n');
+return (0);
 }
