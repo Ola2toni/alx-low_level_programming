e9f91886a207f41a63e33a2a5fec0cd04909ce11
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-10 13:21:21 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	let number;
 
-	for(number = 48; number <= 57; number++)
+	for (number = 48; number <= 57; number++)
 	{
 		putchar (number);
-		if (number == 57)
+		if (number != 57)
 		{
-			break
-		}
 		putchar (',');
 		putchar (' ');
+		}
 	}
+	
 	putchar ('\n');
 	return (0);
 }
