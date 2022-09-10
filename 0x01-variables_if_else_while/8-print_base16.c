04f4d65805c37e0294f089185fcf27d28f52bdec
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-10 13:37:21 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (success/correct)
  */
-
 int main(void)
 {
 	char base_16;
-
-	for (base_16 = 48; base_16 <= 57; base_16++)
+        for (base_16 = '0'; base_16 <= '9'; base_16++)
+	{
+		putchar (base_16);
+	}
+	for (base_16 = 'a'; base_16 <= 'f'; base_16++)
 	{
 		putchar (base_16);
 	}
