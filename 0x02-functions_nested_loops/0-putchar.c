01525e07020425137528bd10f5f498342c036a99
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-13 15:11:57 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,21 @@
 
 /**
  * main - print putchar
- * Return: always 0
+ *
+ * Return: Always 0 success
  */
-
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-        return (0);
-
+	return (0);
 }
 
