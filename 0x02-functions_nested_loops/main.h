3db696ee93107028ed819d9b7d45b848a061d31c
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-13 14:03:45 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERATON_H_INCLUDED
 #define HOLBERATON_H_INCLUDED
 
-int_putchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
