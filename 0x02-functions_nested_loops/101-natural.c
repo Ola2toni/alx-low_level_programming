7a552b7ea0b290491ef976fefdf6dcf86e2b9155
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-14 20:42:08 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int mult = 0;
-	int sum = 0;
+int mult = 0;
+int sum = 0;
 
-	while (mult < 1024)
+while (mult < 1024)
+{
+	if (mult % 3 == 0 || mult % 5 == 0)
 	{
-		if (mult % 3 == 0 || mult % 5 == 0)
-		{
-			 sum += mult;
-		 }
-		mult++;
-		}
-		printf("%d\n", sum);
-		return (0);
-	}
+		 sum += mult;
+	 }
+	mult++;
+}
+	printf("%d\n", sum);
+	return (0);
+}
