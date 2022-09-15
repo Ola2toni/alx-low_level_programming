c5319275ed851a347e3efe126ba4fa14c92ea214
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-15 14:26:08 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,15 +16,15 @@ void print_diagonal(int n)
 		{
 			for (space = 0; space < len; space++)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
-			putchar('\\');
+			_putchar('\\');
 
 			if (len == (n - 1))
 			{
 				continue;
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
