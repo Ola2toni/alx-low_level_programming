b5281ddd474207b88d37adf8343df545eb335f9f
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-15 21:49:40 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints prime factors
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i, n;
+
+	n = 612852475143;
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n% i == 0) && (n != 1))
+		{
+			n = n / i;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
