62fdc70eed97620838582f0ec97860a9e9a5a3df
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-22 14:41:10 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /*
  * print_line - prints a s bytes of a buffer
  * @c: buffer to print
@@ -8,7 +7,6 @@
  * @l: line of buffer to print
  * Return: void
  */
-
 void print_line(char *c, int s, int l)
 {
 int j, k;
@@ -30,7 +28,6 @@ else
 putchar('.');
 }
 }
-
 /*
  * print_buffer - prints a buffer
  * @b: buffer to print
