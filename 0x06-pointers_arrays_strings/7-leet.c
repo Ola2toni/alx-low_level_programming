5b75c589556105b4e1f08725bcdda12998b7a717
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-22 05:43:28 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,36 @@
 #include "main.h"
 /**
- * leet - encodes a string into 1337
- * @s: input string.
- * Return: the pointer dest.
- */    
-char *leet(char *str)
+ * leet - encodes a string into 1337.
+ * letters a and A are replaced by 4.
+ * Letters e and E are replaced by 3
+ * Letters o and O are replaced by 0
+ * Letters t and T are replaced by 7
+ * Letters l and L are replaced by 1
+ * @s: pointer to string.
+ * 
+ * Return: pointer to s.
+ */
+char *leet(char *s)
 {
-	int count = 0, i;
-	int lower_case[] = {97, 101, 111, 116, 108};
-	int upper_case[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+int stringCount, leetCount;
+char leetLetters[] = "aAeEoOtTlL";
+char leetNums[] = "4433007711";
 
-
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
-			{
-				*(s + count) = numbers[i];
-				break;
-			}
-		}
-		count++;
-	}
-
-	return (s);
+/*  scan through string */
+stringCount = 0;
+while (s[stringCount] != '\0')
+/* check whether leetLetter is found */
+{
+leetCount = 0;
+while (leetCount < 10)
+{
+if (leetLetters[leetCount] == s[stringCount])
+{
+	s[stringCount] = leetNums[leetCount];
+}
+leetCount++;
+}
+stringCount++;
+}
+return (s);
 }
