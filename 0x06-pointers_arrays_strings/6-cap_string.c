945dfcecec0fd3c1f2dc108c80c5ad9f8a72dee1
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-22 17:47:10 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,9 @@ char *cap_string(char *str)
 {
 int index = 0;
 
-while (!(str[index] >= 'a' && str[index] <= 'z'))
+while (str[++index])
 {
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
 if (str[index - 1] == ' ' ||
