2556f796016a571d18074229b8190cec359a8e93
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-26 13:18:59 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 /**
  * _memset - fills memory with a constant byte.
  * @s: source string
- * @b: the constant byte fot filling
+ * @b: the constant byte for filling
  * @n: length of buffer
- * Return: new string (s)
+ * Return: new string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
