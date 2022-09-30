672595c0e9c64c8c9e794125a9131862ed9a90a2
Ola2toni <davidolawuyi2003@gmail.com>
2022-09-30 07:37:42 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Function that returns the string length
+ *
+ * @s: this is the input string
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+		return (index);
+}
