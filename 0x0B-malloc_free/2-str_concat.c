eed19072c4fe27668ec6c7e4fa4f2d20adfec6f8
Ola2toni <davidolawuyi2003@gmail.com>
2022-10-03 04:26:50 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@
 char *str_concat(char *s1, char *s2)
 {
 unsigned int len1, len2, size, i, j;
-char *nstr
+char *nstr;
 
 if (s1 == NULL)
 s1 = "";
@@ -23,7 +23,7 @@ len1 = 0;
 while (s1[len1] != '\0')
 len1++;
 len2 = 0;
-while (s2[len2] = '\0')
+while (s2[len2] != '\0')
 len2++;
 
 size = len1 + len2;
