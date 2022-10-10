d19f6ec9265a0b747f7dfb52b119389743b6e91f
Ola2toni <davidolawuyi2003@gmail.com>
2022-10-10 14:47:11 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i <= lenN; i++)
 		n_dog->name[i] = name[i];
-	for (i= 0; i<= lenO; i++)
+	for (i = 0; i <= lenO; i++)
 		n_dog->owner[i] = owner[i];
 	n_dog->age = age;
 	return (n_dog);
