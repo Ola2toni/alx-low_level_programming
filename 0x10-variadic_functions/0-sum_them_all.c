32d6dbc612dfa2bd4c1af179ee232b94ffa632e6
Ola2toni <davidolawuyi2003@gmail.com>
2022-10-13 04:46:00 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
  * @n: number of arguments to be summed
  * Return: sum or 0
  */
-int sum_them_all(const unsigned int n, ..)
+int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0, i;
 	va_list arglist;
